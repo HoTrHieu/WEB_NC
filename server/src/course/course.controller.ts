00ca8eb1af639f51ca0e3e4259267a4e9ebb18ab
tiendung1510 <tiendung1510@outlook.com.vn>
2021-02-24 00:33:14 +0700

    update course service

--- server/src/course/course.controller.ts
+++ server/src/course/course.controller.ts
@@ -1,16 +1,19 @@
 import {
   Body,
-  Controller, Param, Post, Put, Request
+  ClassSerializerInterceptor,
+  Controller, Get, Param, Post, Put, Query, Request, UseInterceptors
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Role } from 'src/shared/decorators/role.decorator';
 import { BooleanResponse } from 'src/shared/dtos/boolean-response.dto';
+import { PagingResponse } from 'src/shared/dtos/paging-response.dto';
 import { StdResponse } from 'src/shared/dtos/std-response.dto';
 import { UpdateStatusRequest } from 'src/shared/dtos/update-status-request.dto';
 import { Course } from 'src/shared/entities/course.entity';
 import { StdResponseCode } from 'src/shared/enums/std-response-code';
 import { UserRole } from 'src/shared/enums/user-role';
 import { CourseService } from './course.service';
+import { CourseSearchRequest } from './dto/course-search-request.dto';
 
 @ApiTags('Course')
 @Controller('/course')
@@ -18,6 +21,26 @@ export class CourseController {
   constructor(
     private courseService: CourseService
   ) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/search')
+  @ApiResponse({
+    type: PagingResponse,
+  })
+  @ApiBearerAuth()
+  search(@Query() request: CourseSearchRequest) {
+    return this.courseService.search(request);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:courseId')
+  @ApiResponse({
+    type: Course,
+  })
+  @ApiBearerAuth()
+  getDetail(@Param('courseId') courseId: number) {
+    return this.courseService.getDetail(courseId);
+  }
 
   @Role(UserRole.TEACHER)
   @Post('/')
